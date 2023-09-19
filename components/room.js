793f99a53108c5837582a2b2dead624a49010efb
ys <shono@themassage.jp>
2023-09-19 11:55:41 +0900

    Update room.js

--- components/room.js
+++ components/room.js
@@ -322,7 +322,7 @@ const Room = () => {
           e.target.style.transform = e.transform;
         }}
       />
-      <div className="board pixel-shadow" id="board_01" ref={resizeTarget} style={{ width: isRoomOpen ? "600px" : "300px", height: isRoomOpen ? "600px" : "300px" }}>
+      <div className="board pixel-shadow" id="board_01" ref={resizeTarget}>
         {!isRoomOpen ? (
           <RoomDefault toggleRoom={toggleRoom} /*dragTarget={dragTarget}*/ />
         ) : (
