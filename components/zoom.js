f313dcbe7b0e7cb2dc002489a1a5b573c7de94a0
ys <shono@themassage.jp>
2023-09-20 15:03:28 +0900

    spの表示を修正

--- components/zoom.js
+++ components/zoom.js
@@ -1,10 +1,10 @@
 const Zoom = (Props) => {
   const zoomin = (e) => {
-    Props.setZoom((prevCount) => prevCount + 0.1);
+    Props.setZoom((prevCount) => prevCount + 0.2);
   };
   const zoomout = (e) => {
     if (Props.zoom > 0.4) {
-      Props.setZoom((prevCount) => prevCount - 0.1);
+      Props.setZoom((prevCount) => prevCount - 0.2);
     }
   };
 
