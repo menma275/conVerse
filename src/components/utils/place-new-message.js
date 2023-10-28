3bdb7ade4d01937c7f78710de627e414f3ee9260
ys <shono@themassage.jp>
2023-10-28 22:44:42 +0900

    postIDの仕様を変更

--- src/components/utils/place-new-message.js
+++ src/components/utils/place-new-message.js
@@ -3,13 +3,12 @@ import { getRandomPalette } from "@/components/utils/utils";
 import { CARD_PALETTE } from "@/components/utils/card-palette";
 import { getNoteFromYPosition } from "@/components/utils/get-note-from-y-position";
 import { playSoundForEmojiCategory } from "@/components/sound/sound-generator";
+import { v4 as uuidv4 } from "uuid";
 
 export const placeNewMessage = (e, setAllCards, userId, spaceId, isAddingCard, container, zoom, message, setMessage) => {
   if (!isAddingCard || !message) return null;
 
   const palette = getRandomPalette(CARD_PALETTE);
-  const cardnum = container.childElementCount;
-  console.log("cardnum", cardnum);
   const containerRect = container.getBoundingClientRect();
   const newX = (e.clientX - containerRect.left) / zoom;
   const newY = (e.clientY - containerRect.top) / zoom;
@@ -19,7 +18,7 @@ export const placeNewMessage = (e, setAllCards, userId, spaceId, isAddingCard, c
   const msg = {
     userId: userId,
     spaceId: spaceId,
-    postId: cardnum,
+    postId: uuidv4(),
     text: message,
     pos: { x: newX, y: newY },
     scale: [1, 1],
