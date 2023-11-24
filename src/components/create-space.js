5fcc4c4d38fde6fa9de15374e5c026d1f993c0ba
menma275 <ohsusie9@gmail.com>
2023-11-24 14:13:18 +0900

    create spaceのスタイル調整

--- src/components/create-space.js
+++ src/components/create-space.js
@@ -13,7 +13,10 @@ const CreateSpace = (props) => {
   const dragTarget = useRef(null);
   const moveableRef = useRef(null);
   const isMobile = useMediaQuery({ query: "(max-width: 720px)" });
-  const [boardSize, setBoardSize] = useState({ width: isMobile ? "90%" : "500px", height: "500px" });
+  const [boardSize, setBoardSize] = useState({
+    width: isMobile ? "90%" : "500px",
+    height: "500px",
+  });
   const [spaceVisible, setSpaceVisible] = useState(false); // space コンポーネントの表示状態を管理
   const [createdSpaceData, setCreatedSpaceData] = useState(null);
   const { userId } = useContext(UserIdContext);
@@ -74,14 +77,38 @@ const CreateSpace = (props) => {
   const saveSpace = async (event) => {
     event.preventDefault();
 
-    const { userId, name, description, spaceType, sounds, messageDesign, resizable, genId, startTime, duration } = formData;
+    const {
+      userId,
+      name,
+      description,
+      spaceType,
+      sounds,
+      messageDesign,
+      resizable,
+      genId,
+      startTime,
+      duration,
+    } = formData;
     console.log("Data to be sent:", formData); // データをログに出力
 
     // ここでAPIにデータを送信
     try {
       const res = await fetch(`${baseUrl}/api/space`, {
         method: "POST",
-        body: JSON.stringify({ info: { userId, name, description, spaceType, sounds, messageDesign, resizable, genId, startTime, duration } }),
+        body: JSON.stringify({
+          info: {
+            userId,
+            name,
+            description,
+            spaceType,
+            sounds,
+            messageDesign,
+            resizable,
+            genId,
+            startTime,
+            duration,
+          },
+        }),
         headers: {
           "Content-Type": "application/json",
         },
@@ -91,7 +118,19 @@ const CreateSpace = (props) => {
         // リソースの作成が成功した場合
         const responseData = await res.json(); // レスポンスデータをJSONとしてパース
         console.log("Space created successfully!", responseData);
-        setCreatedSpaceData({ userId, name, description, spaceType, sounds, messageDesign, resizable, genId, startTime, duration, spaceId: responseData.space.spaceId });
+        setCreatedSpaceData({
+          userId,
+          name,
+          description,
+          spaceType,
+          sounds,
+          messageDesign,
+          resizable,
+          genId,
+          startTime,
+          duration,
+          spaceId: responseData.space.spaceId,
+        });
         console.log("CreateSpace", responseData.space);
         // space コンポーネントを表示
         setSpaceVisible(true);
@@ -130,7 +169,10 @@ const CreateSpace = (props) => {
   return (
     <>
       {spaceVisible ? ( // spaceVisible の値に応じて表示内容を切り替え
-        <Space spaceInfo={createdSpaceData} spaceId={createdSpaceData.spaceId} />
+        <Space
+          spaceInfo={createdSpaceData}
+          spaceId={createdSpaceData.spaceId}
+        />
       ) : (
         <>
           <Moveable
@@ -155,7 +197,15 @@ const CreateSpace = (props) => {
             }}
             ref={moveableRef}
           />
-          <motion.div animate={controls} onMouseDown={handleMouseDownOrTouchStart} style={style} className="board pixel-shadow absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" id="create-new-space" onAnimationComplete={() => updateRect()} ref={resizeTarget}>
+          <motion.div
+            animate={controls}
+            onMouseDown={handleMouseDownOrTouchStart}
+            style={style}
+            className="board pixel-shadow absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+            id="create-new-space"
+            onAnimationComplete={() => updateRect()}
+            ref={resizeTarget}
+          >
             <div className="board-header pixel-shadow" ref={dragTarget}>
               <div className="board-header-set">
                 <h1>Create New Space</h1>
@@ -166,40 +216,109 @@ const CreateSpace = (props) => {
             </div>
             <div className="board-content">
               <form onSubmit={saveSpace} className="block pt-4">
-                <div className="rounded-lg border-2 border-[var(--accent)] mb-2 overflow-hidden">
-                  <input id="name" className="block w-full" type="text" name="name" placeholder="Input Space Name" value={formData.name} onChange={handleInputChange} required />
+                {/* スペース名 */}
+                <div className="rounded-lg border border-[var(--accent)] mb-2 overflow-hidden">
+                  <input
+                    id="name"
+                    className="p-2 block w-full text-sm bg-[var(--cream)] outline-none font-sans"
+                    type="text"
+                    name="name"
+                    placeholder="Input Space Name"
+                    value={formData.name}
+                    onChange={handleInputChange}
+                    required
+                  />
                 </div>
-                <div className="rounded-lg border-2 border-[var(--accent)] mb-2 overflow-hidden">
-                  <textarea id="description" name="description" rows="6" placeholder="Input Description" value={formData.description} onChange={handleInputChange} className="block w-full" />
+                {/* スペース説明 */}
+                <div className="rounded-lg border border-[var(--accent)] mb-2 overflow-hidden font-sans">
+                  <textarea
+                    id="description"
+                    name="description"
+                    rows="6"
+                    placeholder="Input Description"
+                    value={formData.description}
+                    onChange={handleInputChange}
+                    className="p-2 block w-full text-sm bg-[var(--cream)] outline-none"
+                  />
                 </div>
-                <select name="spaceType" value={formData.spaceType} onChange={handleInputChange} required className="mb-2">
-                  <option value="default">Emoji Space</option>
-                </select>
-                <label className="block mb-2">
-                  <input type="checkbox" name="sounds" checked={formData.sounds} onChange={handleInputChange} className="mr-4" />
-                  Sound
+                {/* スペースタイプ */}
+                <div className="flex justify-between my-3 ">
+                  <label className="text-xs my-auto">Space Type</label>
+                  <select
+                    name="spaceType"
+                    value={formData.spaceType}
+                    onChange={handleInputChange}
+                    required
+                    className="text-xs p-1 bg-[var(--cream)] border border-[var(--accent)] rounded-lg font-sans"
+                  >
+                    <option value="default">Emoji Space</option>
+                  </select>
+                </div>
+                {/* サウンド設定 */}
+                <label className="my-2 flex justify-between">
+                  <p className="text-xs">Enable Sounds</p>
+                  <input
+                    type="checkbox"
+                    name="sounds"
+                    checked={formData.sounds}
+                    onChange={handleInputChange}
+                  />
                 </label>
-                <select name="messageDesign" value={formData.messageDesign} onChange={handleInputChange} required className="mb-2">
-                  <option value="card">Card</option>
-                  <option value="nocard">None</option>
-                </select>
-                <label className="block mb-2">
-                  <input type="checkbox" name="resizable" checked={formData.resizable} onChange={handleInputChange} className="mr-4" />
-                  Resizable
-                </label>
-                <select name="genId" value={formData.genId} onChange={handleInputChange} required className="mb-2">
-                  <option value="" disabled>
-                    Select GenArt
-                  </option>
-                  <option value="samuelyan">Samuel YAN</option>
-                  <option value="sakamura">sakamura</option>
-                </select>
-                {/* 'enableStartTime' チェックボックス */}
-                <div className="mb-2">
-                  <label htmlFor="enableStartTime" className="block mb-1">
-                    Enable Start Time:
+                {/* メッセージデザイン */}
+                <div className="flex justify-between my-3">
+                  <p className="text-xs my-auto">Message Design</p>
+                  <select
+                    name="messageDesign"
+                    value={formData.messageDesign}
+                    onChange={handleInputChange}
+                    required
+                    className="text-xs p-1 bg-[var(--cream)] border border-[var(--accent)] rounded-lg font-sans"
+                  >
+                    <option value="card">Card</option>
+                    <option value="nocard">None</option>
+                  </select>
+                </div>
+                {/* リサイズ設定 */}
+                <div className="flex justify-between my-3">
+                  <p className="text-xs my-auto ">Resizable</p>
+                  <label className="">
+                    <input
+                      type="checkbox"
+                      name="resizable"
+                      checked={formData.resizable}
+                      onChange={handleInputChange}
+                    />
                   </label>
-                  <input id="enableStartTime" name="enableStartTime" type="checkbox" checked={enableStartTime} onChange={handleCheckboxChange} />
+                </div>
+                {/* GenArtタイプ */}
+                <div className="flex justify-between my-3">
+                  <p className="text-xs my-auto">Choose Generative Art</p>
+                  <select
+                    name="genId"
+                    value={formData.genId}
+                    onChange={handleInputChange}
+                    required
+                    className="text-xs p-1 bg-[var(--cream)] border border-[var(--accent)] rounded-lg font-sans"
+                  >
+                    <option value="" disabled>
+                      Select GenArt
+                    </option>
+                    <option value="samuelyan">Samuel YAN</option>
+                    <option value="sakamura">sakamura</option>
+                  </select>
+                </div>
+                {/* 'enableStartTime' チェックボックス */}
+                <div className="mt-3 flex justify-between">
+                  <label htmlFor="enableStartTime" className="text-xs my-auto">
+                    Enable Start Time
+                  </label>
+                  <input
+                    id="enableStartTime"
+                    name="enableStartTime"
+                    type="checkbox"
+                    checked={enableStartTime}
+                    onChange={handleCheckboxChange}
+                  />
                 </div>
 
                 {/* 条件付きで開始時間と持続時間の入力フィールドを表示 */}
@@ -209,18 +328,38 @@ const CreateSpace = (props) => {
                       <label htmlFor="startTime" className="block mb-1">
                         Start Time:
                       </label>
-                      <input id="startTime" type="time" name="startTime" value={formData.startTime} onChange={handleInputChange} required />
+                      <input
+                        id="startTime"
+                        type="time"
+                        name="startTime"
+                        value={formData.startTime}
+                        onChange={handleInputChange}
+                        required
+                      />
                     </div>
 
                     <div className="mb-2">
                       <label htmlFor="duration" className="block mb-1">
                         Duration (minutes):
                       </label>
-                      <input id="duration" type="number" name="duration" value={formData.duration} onChange={handleInputChange} min="1" required />
+                      <input
+                        id="duration"
+                        type="number"
+                        name="duration"
+                        value={formData.duration}
+                        onChange={handleInputChange}
+                        min="1"
+                        required
+                      />
                     </div>
                   </>
                 )}
-                <input id="submit" className="pixel-shadow absolute bottom-4 left-1/2 -translate-x-1/2" type="submit" value="Create" />
+                <input
+                  id="submit"
+                  className="pixel-shadow absolute bottom-4 left-1/2 -translate-x-1/2"
+                  type="submit"
+                  value="Create"
+                />
               </form>
             </div>
           </motion.div>
